60dda4db099ac1775d9c5c8134754f436ea85998
BethanyRau-consulting <bethanyrau8@gmail.com>
2025-02-10 13:02:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { onSnapshot } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
 
 import {
   getFirestore,
